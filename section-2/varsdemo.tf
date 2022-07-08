a01be5b9fd9776bd7fdf8fa556e3c9eba3431467
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-07-08 19:26:02 +1000

    Updated SG

---
+++ section-2/varsdemo.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "hui-sg" {
+  name        = "hui-inbound"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpn_ip]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpn_ip]
+  }
+  
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 53
+    to_port          = 53
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpn_ip]
+  } 
+}
