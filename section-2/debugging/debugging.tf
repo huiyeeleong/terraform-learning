18de643a966356d5a56d0479eebd20fdc0a4d923
Hui Yee Leong <66815986+huiyeeleong@users.noreply.github.com>
2022-07-09 22:40:19 +1000

    Added Section 2

---
+++ section-2/debugging/debugging.tf
@@ -0,0 +1,2 @@
+export TF_LOG_PATH=/tmp/crash.log
+export TF_LOG=TRACE
--- section-2/functions/functions.tf
+++ section-2/functions/functions.tf
@@ -7,15 +7,15 @@ variable "region" {
 }
 
 variable "tags" {
-  type = list
-  default = ["firstec2","secondec2"]
+  type    = list(any)
+  default = ["firstec2", "secondec2"]
 }
 
 variable "ami" {
-  type = map
+  type = map(any)
   default = {
-    "us-east-1" = "ami-0323c3dd2da7fb37d"
-    "us-west-2" = "ami-0d6621c01e8c2de2c"
+    "us-east-1"  = "ami-0323c3dd2da7fb37d"
+    "us-west-2"  = "ami-0d6621c01e8c2de2c"
     "ap-south-1" = "ami-0470e33cd681b2476"
   }
 }
@@ -26,14 +26,14 @@ resource "aws_key_pair" "loginkey" {
 }
 
 resource "aws_instance" "app-dev" {
-   ami = lookup(var.ami,var.region)
-   instance_type = "t2.micro"
-   key_name = aws_key_pair.loginkey.key_name
-   count = 2
+  ami           = lookup(var.ami, var.region)
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.loginkey.key_name
+  count         = 2
 
-   tags = {
-     Name = element(var.tags,count.index)
-   }
+  tags = {
+    Name = element(var.tags, count.index)
+  }
 }
 
 
